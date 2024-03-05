a4fc0ece314868c3904c8cee7bf196f9ccd150d9
chloe <chloe@DESKTOP-MHQ5M5I>
2024-03-05 19:39:02 -0300

    Adicionar arquivos de projeto.

---
+++ Ex. 2/Program.cs
@@ -0,0 +1,17 @@
+﻿double[] sal2 = new double[5];
+
+for (int i = 0; i < 5; i++)
+{
+    Console.Write($"Digite o salário da {i + 1}ª pessoa: ");
+    sal2[i] = double.Parse(Console.ReadLine());
+    if (sal2[i] < 1000)
+    {
+        sal2[i] *= 1.1;
+    }
+}
+
+Console.WriteLine("Lista de salários atualizada:");
+foreach (double sal in sal2)
+{
+    Console.WriteLine(sal);
+}
